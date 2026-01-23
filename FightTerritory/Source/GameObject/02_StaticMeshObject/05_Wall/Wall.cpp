f64e77eeebbd8bbb39dab4a7ba99e1cb7733eb68
game <game@R200-18>
2026-01-23 14:09:21 +0900

    壁作成用の壁表示できた

---
+++ FightTerritory/Source/GameObject/02_StaticMeshObject/05_Wall/Wall.cpp
@@ -0,0 +1,32 @@
+#include "Wall.h"
+
+#include "System//00_Manager//01_StaticMeshManager//StaticMeshManager.h"
+
+Wall::Wall()
+	: StaticMeshObject	()
+{
+	//壁のアタッチ.
+	AttachMesh(*StaticMeshManager::GetInstance()->GetMeshInstance(StaticMeshManager::CMeshList::Wall));
+
+	//壁の位置(仮).
+	D3DXVECTOR3 Pos = { -20.0f,1.5f,0.0f };
+	//壁のサイズ(仮).
+	D3DXVECTOR3 Scale = { 5.0f,5.0f,5.0f};
+
+	SetPosition(Pos);
+	SetScale(Scale);
+}
+
+Wall::~Wall()
+{
+}
+
+void Wall::Update()
+{
+	StaticMeshObject::Update();
+}
+
+void Wall::Draw()
+{
+	StaticMeshObject::Draw();
+}
--- FightTerritory/Source/GameObject/02_StaticMeshObject/05_Wall/Wall.h
+++ FightTerritory/Source/GameObject/02_StaticMeshObject/05_Wall/Wall.h
@@ -1,1 +1,23 @@
 #pragma once
+#include "GameObject//02_StaticMeshObject//StaticMeshObject.h"
+
+class StaticMeshManager;
+
+/***************************************************************************
+*	壁クラス.
+*	ToDo : ImGuiとファイルの保存を使用して実装していく.
+**/
+
+class Wall final
+	: public StaticMeshObject
+{
+public:
+	Wall();
+	~Wall() override;
+
+	void Update() override;
+	void Draw() override;
+
+private:
+
+};
